881d34ab667f0e2aa4e637835de8b09c3ddbea52
FrancoNos <fat7_2014@hotmail.com>
2023-09-14 20:29:12 -0300

    port con env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 conn.sync({ force: false }).then(() => {
   console.log('¡Se ha conectado la DB!')
-server.listen(PORT, () => {
-  console.log(`Server abierto en el puerto ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server abierto en el puerto ${process.env.PORT}`);
 })
 }).catch(error => console.error(error))
